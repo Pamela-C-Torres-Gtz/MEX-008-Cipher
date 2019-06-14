5566e9bbf5e30d9ad8ffd0c8e5d14ef52f184451
Pamela-C-Torres-Gtz <pamelatgtz@gmail.com>
2019-06-13 19:08:00 -0500

    privera version

---
+++ src/index.js
@@ -0,0 +1,33 @@
+/*shadowing (sombra) y funcion*/
+/*
+let x ='global';
+funcion f(){
+  let x = 'local';
+  console.log ();
+
+}
+*/
+
+/*trabajo*/
+/* 1.- Obtener el texto*/
+/*implementar el evento click del DOM*/
+const btnCifrar= document.getElementById('btnCifrar').addEventListener('click');
+const texto =document.getElementById('clave');
+/* se guarda la información*/
+let str= [];
+
+let string = 'ABC';
+/*2.-Este es el desplazamiento offset*/
+const encode= function (){
+   /* Cambia a mayusculas todas las letras*/
+  const mayusculas =input.value.toUpperCase ();
+   for (let i = 0; 1 < texto.length; 1 ++){
+/*6. buscar el valor de cada letra en codigo Ascci*/
+    string.charCodeAt(0);
+    /*7 se aplica la formula de michell*/
+    let encode= string  (x -65 + n) % 26 + 65;
+      }
+  }
+}
+     /*9 busco donde almacenar la información*/
+  str=str.resultado;

9d307c034457d2ac9a67af5a884515bbb471cf86
Pamela-C-Torres-Gtz <pamelatgtz@gmail.com>
2019-06-18 17:27:53 -0500

    Agrege funcionalidad a mi boton de cifrado incluyendo la modificación de mi archivo cipher.js e index.js

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,42 @@
 window.cipher = {
-  // ... 
+  encode: (offset,string) => 
+  {
+    let codeModificado = 0;
+    /*declaramos nuestra variable vacia*/
+    let textModificado = '';
+    /* realizamos un ciclo*/
+    for (let i = 0; i < string.length; i ++)
+    {
+     //colocamos formula //
+    //hacer la validadción despues de que hace la conversion de la letra a codigo acci//
+    const ascii = string.charCodeAt (i);
+     if (ascii >= 65 && ascii <=90 )
+       {
+         codeModificado = (ascii-65+offset) % 26 + 65;
+         //textModificado=String.fromCharCode(codeModificado);
+       } else if (ascii >= 97 && ascii <= 122)
+         {
+           codeModificado =(ascii - 97 + offset) % 26 +97;
+           /*para considerar caracters diferente el programa lo puede devolver igual o lo ignora*/
+         }else 
+     {
+      codeModificado = ascii;
+     }
+     //cambia el numero del codigo ascii a letra
+     textModificado += String.fromCharCode(codeModificado);
+    } 
+    /*console.log(codeModificado);
+    console.log()*/0
+  return textModificado;
+  }
+/* en lugar de 3 posiblemente sea offset.value para que */
 };
+
+// funcion de decifrado
+window.cipher = {
+  encode: (offset,string) => 
+  {
+    let codeModificado = 0;
+    /*declaramos nuestra variable vacia*/
+    let textModificado = '';
+    /* realizamos un ciclo*/
--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-//correcccion de codigo completo
+//correcccion de codigo completo mandamo llamar a todos mis inputs
+
+const inputEncode=document.getElementById('txt-cipher');
+const offset= document.getElementById('offset');
+const btnCifrar= document.getElementById('botonCifrar');
+const inputDecode=document.getElementById('input-decoder');
+/* recomendacion de silvana para obtener el valos de nuestra caja con un evento*/
+
+/* inputEncode.addEventListene('keyup', () => {}*/ 
+/*se puede utilizar keyup, keydwon y keypress*/
+/* evento del dom o ejecutar funcion cipher*/
+btnCifrar.addEventListener('click', (e) => {
+  e.preventDefault();
+  console.log(offset.value);
+
+  inputDecode.value =window.cipher.encode(parseInt(offset.value), inputEncode.value)
+
+})

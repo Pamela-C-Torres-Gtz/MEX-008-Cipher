3be985bb6440c12657f52762a124da97edb2a643
Pamela-C-Torres-Gtz <pamelatgtz@gmail.com>
2019-06-19 16:39:37 -0500

    Realice exitosamente la función del metodo de Decodificación. Tambien realice algunos cambios en el archivo de style.css en cuanto al borde de mi caja de presentación. Por último realice mis primeros test y corregi errores(NOTA: tengo algunos detalles con mi test pero lo checare con Couche Jhon)

--- src/cipher.js
+++ src/cipher.js
@@ -1,18 +1,59 @@
 window.cipher = {
-  encode: (offset,string) => 
+   encode: (offset,string) => 
+  {
+    let codeModificado = 0;
+    //declaramos nuestra variable vacia
+    let textModificado = '';
+    // convertir a mayusculas
+    let strCl= string.toUpperCase();
+
+    // realizamos un ciclo
+    for (let i = 0; i < strCl.length; i ++)
+    {
+     //colocamos formula //
+    //hacer la validadción despues de que hace la conversion de la letra a codigo acci//
+    const ascii = strCl.charCodeAt (i);
+     if (ascii >= 65 && ascii <=90 )
+       {
+         codeModificado = (ascii-65+offset) % 26 + 65;
+         //textModificado=String.fromCharCode(codeModificado);
+       } else if (ascii >= 97 && ascii <= 122)
+         {
+           codeModificado =(ascii - 97 + offset) % 26 +97;
+           //para considerar caracters diferente el programa lo puede devolver igual o lo ignora
+         }else 
+     {
+      codeModificado = ascii;
+     }
+     //cambia el numero del codigo ascii a letra
+     textModificado += String.fromCharCode(codeModificado);
+    } 
+    //console.log(codeModificado);
+    //console.log() 0
+  return textModificado;
+  },
+// en lugar de 3 posiblemente sea offset.value para que 
+
+
+// funcion de decifrado
+
+  decode: (offset,string) => 
   {
     let codeModificado = 0;
     /*declaramos nuestra variable vacia*/
     let textModificado = '';
+    // convertir a mayusculas
+    let strCl= string.toUpperCase();
+
     /* realizamos un ciclo*/
-    for (let i = 0; i < string.length; i ++)
+    for (let i = 0; i < strCl.length; i ++)
     {
      //colocamos formula //
     //hacer la validadción despues de que hace la conversion de la letra a codigo acci//
-    const ascii = string.charCodeAt (i);
+    const ascii = strCl.charCodeAt (i);
      if (ascii >= 65 && ascii <=90 )
        {
-         codeModificado = (ascii-65+offset) % 26 + 65;
+         codeModificado = (ascii+65-offset) % 26 + 65;
          //textModificado=String.fromCharCode(codeModificado);
        } else if (ascii >= 97 && ascii <= 122)
          {
@@ -28,15 +69,6 @@ window.cipher = {
     /*console.log(codeModificado);
     console.log()*/0
   return textModificado;
-  }
-/* en lugar de 3 posiblemente sea offset.value para que */
-};
+  },
+}
 
-// funcion de decifrado
-window.cipher = {
-  encode: (offset,string) => 
-  {
-    let codeModificado = 0;
-    /*declaramos nuestra variable vacia*/
-    let textModificado = '';
-    /* realizamos un ciclo*/
--- src/index1.js
+++ src/index1.js
@@ -1,18 +1,18 @@
 //correcccion de codigo completo mandamo llamar a todos mis inputs
 
-const inputEncode=document.getElementById('txt-cipher');
+const inputDecode=document.getElementById('txt-decode');
 const offset= document.getElementById('offset');
-const btnCifrar= document.getElementById('botonCifrar');
-const inputDecode=document.getElementById('input-decoder');
+const btnDecifrar= document.getElementById('botonDecifrar');
+const inputOrigin=document.getElementById('input-origin');
 /* recomendacion de silvana para obtener el valos de nuestra caja con un evento*/
 
 /* inputEncode.addEventListene('keyup', () => {}*/ 
 /*se puede utilizar keyup, keydwon y keypress*/
 /* evento del dom o ejecutar funcion cipher*/
-btnCifrar.addEventListener('click', (e) => {
+btnDecifrar.addEventListener('click', (e) => {
   e.preventDefault();
  // console.log(offset.value);
 
-  inputDecode.value=window.cipher.encode(parseInt(offset.value), inputEncode.value)
+  inputOrigin.value =window.cipher.decode(parseInt(offset.value), inputDecode.value)
 
 })

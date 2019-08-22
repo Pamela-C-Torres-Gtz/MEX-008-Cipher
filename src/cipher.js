b18290c044fabd668abd71dbe3ce7cff7fbb8ffc
Pamela Torres <pamelatgtz@gmail.com>
2019-08-21 19:16:24 -0500

    funcionalidad de JS de Hacker edition de Chipher y depuracion de archivos

--- src/cipher.js
+++ src/cipher.js
@@ -1,74 +1,61 @@
 window.cipher = {
+  //FUNCION DE CIFRADO
    encode: (offset,string) => 
   {
     let codeModificado = 0;
-    //declaramos nuestra variable vacia
-    let textModificado = '';
-    // convertir a mayusculas
-
-
-    // realizamos un ciclo
-    for (let i = 0; i < string.length; i ++)
+    let textModificado = '';  //declaramos nuestra variable vacia
+    for (let i = 0; i < string.length; i ++)  // realizamos un ciclo
     {
-     //colocamos formula //
-    //hacer la validadción despues de que hace la conversion de la letra a codigo acci//
     const ascii = string.charCodeAt (i);
      if (ascii >= 65 && ascii <=90 )
        {
-         codeModificado = (ascii-65+offset) % 26 + 65;
-         //textModificado=String.fromCharCode(codeModificado);
+         codeModificado = (ascii-65+offset) % 26 + 65;  
+         //textModificado=String.fromCharCode(codeModificado);       
        } else if (ascii >= 97 && ascii <= 122)
          {
-           codeModificado =(ascii - 97 + offset) % 26 +97;
-           //para considerar caracters diferente el programa lo puede devolver igual o lo ignora
+           codeModificado =(ascii - 97 + offset) % 26 + 97;  //para considerar caracters diferente el programa lo puede devolver igual o lo ignora   
          }else 
      {
       codeModificado = ascii;
      }
-     //cambia el numero del codigo ascii a letra
-     textModificado += String.fromCharCode(codeModificado);
+     textModificado += String.fromCharCode(codeModificado);  //cambia el numero del codigo ascii a letra
     } 
     //console.log(codeModificado);
-    //console.log() 0
   return textModificado;
   },
-// en lugar de 3 posiblemente sea offset.value para que 
 
+// FUNCION DE DESCIFRADO
+decode: (offsetD,strCl) => 
+{
+  let codeModificado = 0;
+  /*declaramos nuestra variable vacia*/
+  let textModificado = '';
+  // convertir a mayusculas
+ // let strCl= string.toUpperCase();
 
-// funcion de decifrado
-
-  decode: (offset,string) => 
+  /* realizamos un ciclo*/
+  for (let i = 0; i < strCl.length; i ++)
   {
-    let codeModificado = 0;
-    /*declaramos nuestra variable vacia*/
-    let textModificado = '';
-    // convertir a mayusculas
-    let strCl= string.toUpperCase();
-
-    /* realizamos un ciclo*/
-    for (let i = 0; i < strCl.length; i ++)
-    {
-     //colocamos formula //
-    //hacer la validadción despues de que hace la conversion de la letra a codigo acci//
-    const ascii = strCl.charCodeAt (i);
-     if (ascii >= 65 && ascii <=90 )
-       {
-         codeModificado = (ascii+65-offset) % 26 + 65;
-         //textModificado=String.fromCharCode(codeModificado);
-       } else if (ascii >= 97 && ascii <= 122)
-         {
-           codeModificado =(ascii - 97 + offset) % 26 +97;
-           /*para considerar caracters diferente el programa lo puede devolver igual o lo ignora*/
-         }else 
+   //colocamos formula //
+  //hacer la validadción despues de que hace la conversion de la letra a codigo acci//
+  const ascii = strCl.charCodeAt (i);
+   if (ascii >= 65 && ascii <=90 )
      {
-      codeModificado = ascii;
-     }
-     //cambia el numero del codigo ascii a letra
-     textModificado += String.fromCharCode(codeModificado);
-    } 
-    /*console.log(codeModificado);
-    console.log()*/0
-  return textModificado;
-  },
+       codeModificado = (ascii-90-offsetD) % 26 + 90;
+       //textModificado=String.fromCharCode(codeModificado);
+     } else if (ascii >= 97 && ascii <= 122)
+       {
+         codeModificado =(ascii -122 - offsetD) % 26 +122;
+         /*para considerar caracters diferente el programa lo puede devolver igual o lo ignora*/
+       }else 
+   {
+    codeModificado = ascii;
+   }
+   //cambia el numero del codigo ascii a letra
+   textModificado += String.fromCharCode(codeModificado);
+  } 
+  /*console.log(codeModificado);
+  console.log()*/0
+return textModificado;
+},
 }
-
--- src/index.js
+++ src/index.js
@@ -1,18 +1,45 @@
-//correcccion de codigo completo mandamo llamar a todos mis inputs
-
+//declaracion de constantes para el cifrado
 const inputEncode=document.getElementById('txt-cipher');
 const offset= document.getElementById('offset');
-const btnCifrar= document.getElementById('botonCifrar');
-const inputDecode=document.getElementById('input-decoder');
+const inputResult=document.getElementById('input-decoder');
+const btnCifrar= document.getElementById('btnCipher');
+
+//Declaración de constantes para el Descifrado
+const inputDecode=document.getElementById('txt-decode');
+const offsetD= document.getElementById('offsetD');
+const inputOrigin=document.getElementById('input-origin');
+const btnDecifrar= document.getElementById('btnDecipher');
+
+//declaración de variables para el boton de limpiado de pantalla
+let btnLimpiar = document.getElementById('btnLimpiar');
+
 /* recomendacion de silvana para obtener el valos de nuestra caja con un evento*/
 
-/* inputEncode.addEventListene('keyup', () => {}*/ 
-/*se puede utilizar keyup, keydwon y keypress*/
+
 /* evento del dom o ejecutar funcion cipher*/
 btnCifrar.addEventListener('click', (e) => {
   e.preventDefault();
  // console.log(offset.value);
+  inputResult.value=window.cipher.encode(parseInt(offset.value), inputEncode.value)
+})
 
-  inputDecode.value=window.cipher.encode(parseInt(offset.value), inputEncode.value)
 
+// evento del dom o ejecutar funcion Decipher
+btnDecifrar.addEventListener('click', (e) => {
+  e.preventDefault();
+ // console.log(offset.value);
+  inputOrigin.value =window.cipher.decode(parseInt(offsetD.value), inputDecode.value)
+
+})
+
+// evento dom para ejecutar limpiar
+btnLimpiar.addEventListener("click", (limpiar) => {
+  limpiar.preventDefault();
+
+  document.getElementById('txt-cipher').value = "";
+  document.getElementById('offset').value = "";
+  document.getElementById('input-decoder').value = "";
+  document.getElementById('txt-decode').value = "";
+  document.getElementById('offsetD').value = "";
+  document.getElementById('input-origin').value = "";
 })
